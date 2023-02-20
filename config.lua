a506abb79f8e239cfd42c543206fe0a759f9d443
ChouCookieSan <98222012+CookieSan64@users.noreply.github.com>
2023-02-20 09:38:05 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.Price = 50
+
+Config.DrawDistance = 100.0
+Config.MarkerSize   = vector3(1.5, 1.5, 1.0)
+Config.MarkerColor  = {r = 102, g = 102, b = 204}
+Config.MarkerType   = 27
+
+Config.Locale = 'fr'
+
+Config.Shops = {
+	vector3(-1282.05, -1117.698, 6.10),
+	vector3(137.5368, -1707.465, 28.30),
+	vector3(-814.3221, -183.8153, 36.60),
+	vector3(-33.31556, -153.2465, 56.10),
+	vector3(1213.31, -473.3176, 65.25),
+	vector3(1931.559, 3731.03, 31.90),
+	vector3(-277.7294, 6227.224, 30.70),
+	
+	vector3(-553.9382, -583.4738, 40.43023)
+}
